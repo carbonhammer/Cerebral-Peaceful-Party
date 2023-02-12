5df53be60f643b27a524b78a510dbcfdf2237151
Joao Vitor <joaovitoroliveirasoarespereira@gmail.com>
2023-02-12 00:46:13 -0300

    ----------

---
+++ Aula 7 - Operadores aritmeticos/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*
+Os operadores aritmeticos são simbolos utilizados para realizar operações matematicas como:
+*/
+
+int main(){
+	
+	//Soma -> ( + )
+	int numero_1 = 10 + 10; // Resultado : variavel = 20
+	
+	//Subtração-> ( - )
+	int numero_2 = 270 - 120; // Resultado : variavel = 150
+		
+	//Multiplicação -> ( * )
+	int numero_3 = 34 * 2; // Resultado : variavel = 68
+
+	//Divisão -> ( / )
+	int numero_4 = 180 / 4; // Resultado : variavel = 45
+
+	//Módulo -> ( % )
+	int numero_5 = 100 % 3; // Resultado : variavel = 1
+	
+	return 0;
+}
+
+/*
+A linguagem de programação C++ ainda permite operadores aritimeticos para operações 
+bit a bit mas este é um conceit desnecessário nesta parte do curso.
+*/
